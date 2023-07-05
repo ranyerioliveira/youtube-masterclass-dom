a5a9b29efef88ac60535699dd3f2652ed72e33e2
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-07-05 19:00:20 -0300

    Projeto
    Continuando colocando o evento de teclado

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 
-// get all keys (pegar todas as teclas)
+// ---get all keys (pegar todas as teclas)---
 
 const keys = document.querySelectorAll(".key")
 
 
-//play notes (tocar notas)
-
+//---play notes (tocar notas)---
 //criando uma função
 
-function playNote(event){}
+function playNote(event){
+   //codigo da tecla
 
-// click with mouse (clicar o mouse)
+   //tipo da tecla
 
-// keyboard type  (clicar no teclado)
+   //se a tecla existe
+}
+
+
+// ---click with mouse (clicar o mouse)---
+
+// ---keyboard type  (clicar no teclado)---
 //colocando enventos
-window.addEventListener("keydown", function(event){
-    console.log(event)
-}) // o keydown serve para que toda vez que focar apertada uma tecla no teclad ele vai rodar uma function
+window.addEventListener("keydown", playNote) // o keydown serve para que toda vez que focar apertada uma tecla no teclad ele vai rodar uma function
 
