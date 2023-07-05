87b673bc26b9b7c4e778f6ee4ef103c356c417e3
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-07-05 20:51:28 -0300

    keys
    keys

--- main.js
+++ main.js
@@ -8,15 +8,21 @@ const keys = document.querySelectorAll(".key")
 //criando uma função
 
 function playNote(event){
+
    //codigo da tecla
 
    //tipo da tecla
 
    //se a tecla existe
+    
+   //play midia  
+
 }
 
 
 // ---click with mouse (clicar o mouse)---
+
+keys.forEach()//forEach siginifa para cada 
 
 // ---keyboard type  (clicar no teclado)---
 //colocando enventos
